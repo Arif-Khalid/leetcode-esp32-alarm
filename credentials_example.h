6b992c3abd0feb6f037b51a51b45f1aff7ef6887
arif <arifkhalid99@gmail.com>
2025-09-26 01:44:18 +0800

    chore: add example credentials

---
+++ credentials_example.h
@@ -0,0 +1,8 @@
+#ifndef CREDS_H
+#define CREDS_H
+
+const char* ssid = "YOUR_NETWORK_NAME";
+const char* password = "YOUR_NETWORK_PASSWORD";
+const char* username = "YOUR_LEETCODE_USERNAME";
+
+#endif
