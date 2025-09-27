0ceba26442d1d0807feb74baf78a01f88584117d
arif <arifkhalid99@gmail.com>
2025-09-27 21:00:14 +0800

    fix: error code display on led bar

--- feedback.ino
+++ feedback.ino
@@ -41,27 +41,30 @@ void failureLedSound(){
 }
 
 void retryLed(float progress){
-  digitalWrite(LED_BUILTIN, LOW);
+  if(errorCode == 0){
+    // No error
 
-  // Display progress on LED BAR
-  // First bar signals 0 -> 1 / LED_BAR_COUNT progress
-  // Full bar signals LED_BAR_COUNT - 1 / LED_BAR_COUNT progress
-  progress *= LED_BAR_COUNT;
-  // Prevent array overflow if so happen to have full progress which would light LED_BAR_COUNT bars
-  if(progress >= LED_BAR_COUNT){
-    progress -= 1;
-  }
-  for(int i = 0; i <= progress; ++i){
-    digitalWrite(LED_BAR_PINS[i], HIGH);
-  }
+    // Display progress on LED BAR
+    // First bar signals 0 -> 1 / LED_BAR_COUNT progress
+    // Full bar signals LED_BAR_COUNT - 1 / LED_BAR_COUNT progress
+    progress *= LED_BAR_COUNT;
+    // Prevent array overflow if so happen to have full progress which would light LED_BAR_COUNT bars
+    if(progress >= LED_BAR_COUNT){
+      progress -= 1;
+    }
+    for(int i = 0; i <= progress; ++i){
+      digitalWrite(LED_BAR_PINS[i], HIGH);
+    }
+    
+  }else{
+    // Some error occurred
 
-  if(errorCode > 0){
+    // Blink built in led
     unsigned long currentMillis = millis();
     if(previousMillis > currentMillis){
       // Rollover occured
       previousMillis = 0;
     }
-
     // check if interval has passed
     if (currentMillis - previousMillis >= INTERVAL) {
       previousMillis = currentMillis;   // save last toggle time
@@ -69,7 +72,8 @@ void retryLed(float progress){
       digitalWrite(LED_BUILTIN, isLedOn);    // apply to LED
     }
 
-    // Overwrite LED BAR with error code
+    // set LED BAR with error code
+    setAllLedBar(LOW);
     if(errorCode >= LED_BAR_COUNT){
       // Exceed error code display capabilities fallback (Maybe can be improved with binary rep.)
       setAllLedBar(HIGH);
